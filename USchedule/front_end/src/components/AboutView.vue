d880d6753aa3a4b9d391d1fea86d66773c527b4e
EgorovaElena <elenaegorova4922@gmail.com>
2022-06-08 22:18:57 +0300

    Информация О нас. Редактирование Главной страницы

--- USchedule/front_end/src/components/AboutView.vue
+++ USchedule/front_end/src/components/AboutView.vue
@@ -1,10 +1,14 @@
 <template>
-    <div >
-        Работали над приложением:
+    <div class="container">
+        <p>Работали над приложением:</p>
+        <p>Хрипунов В.А. @TNL_love_silence</p>
+        <p> Егорова Е.А. @elenka_enot</p>
+        <p>Астанина Д.А. @tekkon_kinkreet</p>
+        <p>Гермоненко Е.А. @egermone</p>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
--- USchedule/front_end/src/components/HomeView.vue
+++ USchedule/front_end/src/components/HomeView.vue
@@ -1,10 +1,25 @@
 <template>
-    <div class="bg-warning">
-        hello world
+    <header class="py-5 border-bottom">
+        <div class="container-xxl bd-gutter pt-md-1 pb-md-4">
+            <div class="">
+                <h3 class="bd-title mt-0">Приветствуем вас!</h3>
+                <p class="bd-lead">Найдите свое расписание из 1000 других</p>
+
+
+            </div>
+        </div>
+    </header>
+    <div class="row container">
+        <div class="col">
+            <h3>Легко читаемый дизайн</h3>
+        </div>
+        <div class="col">
+            <h3>Актуальное расписание с последними изменениями</h3>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
