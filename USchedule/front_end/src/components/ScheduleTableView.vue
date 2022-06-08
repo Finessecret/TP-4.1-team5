a6010e1996d38a5d1623ac7bf923464319323274
EgorovaElena <elenaegorova4922@gmail.com>
2022-06-08 23:19:44 +0300

    исправление презентации

--- USchedule/front_end/src/components/ScheduleTableView.vue
+++ USchedule/front_end/src/components/ScheduleTableView.vue
@@ -26,6 +26,7 @@
                             <th scope="col">Предмет</th>
                             <th scope="col">Преподаватель</th>
                             <th scope="col">Аудитория</th>
+                            <th scope="col">Вместительность</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,6 +35,7 @@
                             <td>{{ item.subject }}</td>
                             <td>{{ item.teacher }}</td>
                             <td>{{ item.classroom }}</td>
+                            <td>{{ item.placecount }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -46,6 +48,8 @@
                             <th scope="col">Предмет</th>
                             <th scope="col">Преподаватель</th>
                             <th scope="col">Аудитория</th>
+                            <th scope="col">Вместительность</th>
+
                         </tr>
                     </thead>
                     <tbody>
@@ -54,6 +58,7 @@
                             <td>{{ item.subject }}</td>
                             <td>{{ item.teacher }}</td>
                             <td>{{ item.classroom }}</td>
+                            <td>{{ item.placecount }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -71,7 +76,8 @@ export default {
     data() {
         return {
             searchText: null,
-            shedule: [{
+            shedule: [
+                /*{
                 day: 'Понедельник', numerator:
                     [
                         { time: '8:00', subject: 'Военная подготовка', teacher: null, classroom: null },
@@ -81,7 +87,7 @@ export default {
                         { time: '15:10', subject: 'Военная подготовка', teacher: null, classroom: null },
                         { time: '16:55', subject: 'Военная подготовка', teacher: null, classroom: null },
                         { time: '18:40', subject: 'Военная подготовка', teacher: null, classroom: null },
-                        { time: '20:10', subject: 'Военная подготовка', teacher: null, classroom: null },
+                        { time: '20:10', subject: 'Военная подготовка', teacher: null, classroom: null, placecount: 150 },
 
                     ]
                 , denominator: [
@@ -93,62 +99,62 @@ export default {
                     { time: '15:10', subject: 'Военная подготовка', teacher: null, classroom: null },
                     { time: '16:55', subject: 'Военная подготовка', teacher: null, classroom: null },
                     { time: '18:40', subject: 'Военная подготовка', teacher: null, classroom: null },
-                    { time: '20:10', subject: 'Военная подготовка', teacher: null, classroom: null },
+                    { time: '20:10', subject: 'Военная подготовка', teacher: null, classroom: null, placecount: 150 },
                 ]
 
-            },
-            {
-                day: 'Вторник', numerator:
-                    [
+            },*/
+                {
+                    day: 'Понедельник', numerator:
+                        [
+                            { time: '8:00 - 9:35', subject: 'Технологии программирования', teacher: 'Зенин К.В.', classroom: '479', placecount: 150 },
+                            { time: '9:45 - 11:20', subject: 'Квантовая теория', teacher: 'Стадная Н.П.', classroom: '385', placecount: 25 },
+                            { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null, placecount: null },
+                            { time: '13:25 - 15:00', subject: null, teacher: null, classroom: null, placecount: null },
+                            { time: '15:10 - 16:45', subject: 'РБЗ', teacher: 'Матвеев М.Г.', classroom: 'ДО', placecount: 50 },
+                            { time: '16:55 - 18:30', subject: 'SAP R3', teacher: 'Илларионов И.В.', classroom: 'ДО', placecount: 30 },
+                            { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null, placecount: null },
+                            { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null, placecount: null },
+
+                        ]
+                    , denominator: [
+
+                        { time: '8:00 - 9:35', subject: 'Технологии программирования', teacher: 'Зенин К.В.', classroom: '479', placecount: 150 },
+                        { time: '9:45 - 11:20', subject: 'Квантовая теория', teacher: 'Стадная Н.П.', classroom: '385', placecount: 25 },
+                        { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null, placecount: null },
+                        { time: '13:25 - 15:00', subject: 'ИТ', teacher: 'Михайлов Е.М.', classroom: 'ДО', placecount: 50 },
+                        { time: '15:10 - 16:45', subject: 'РБЗ', teacher: 'Матвеев М.Г.', classroom: 'ДО', placecount: 30 },
+                        { time: '16:55 - 18:30', subject: 'SAP R3', teacher: 'Илларионов И.В.', classroom: 'ДО', placecount: 30 },
+                        { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null, placecount: null },
+                        { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null, placecount: null },
+                    ]
+
+                },
+                {
+                    day: 'Среда', numerator:
+                        [
+                            { time: '8:00 - 9:35', subject: 'ИСиС', teacher: 'Коваль А.С.', classroom: '316п' },
+                            { time: '9:45 - 11:20', subject: 'ФИЗВОСПИТАНИЕ', teacher: 'Щеглова Е.В.', classroom: 'центральный корпус' },
+                            { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null },
+                            { time: '13:25 - 15:00', subject: 'ЯП Джава', teacher: 'Самойлов Н.К', classroom: 'ДО' },
+                            { time: '15:10 - 16:45', subject: 'КТ', teacher: 'Запрягаев С.А.', classroom: 'ДО' },
+                            { time: '16:55 - 18:30', subject: 'ИТ', teacher: 'Михайлов Е.М.', classroom: 'ДО' },
+                            { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null },
+                            { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null },
+
+                        ]
+                    , denominator: [
+
                         { time: '8:00 - 9:35', subject: 'Технологии программирования', teacher: 'Зенин К.В.', classroom: '479' },
                         { time: '9:45 - 11:20', subject: 'Квантовая теория', teacher: 'Стадная Н.П.', classroom: '385' },
                         { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null },
                         { time: '13:25 - 15:00', subject: null, teacher: null, classroom: null },
-                        { time: '15:10 - 16:45', subject: 'РБЗ', teacher: 'Матвеев М.Г.', classroom: 'ДО' },
-                        { time: '16:55 - 18:30', subject: 'SAP R3', teacher: 'Илларионов И.В.', classroom: 'ДО' },
-                        { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null },
-                        { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null },
-
-                    ]
-                , denominator: [
-
-                    { time: '8:00 - 9:35', subject: 'Технологии программирования', teacher: 'Зенин К.В.', classroom: '479' },
-                    { time: '9:45 - 11:20', subject: 'Квантовая теория', teacher: 'Стадная Н.П.', classroom: '385' },
-                    { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null },
-                    { time: '13:25 - 15:00', subject: 'ИТ', teacher: 'Михайлов Е.М.', classroom: 'ДО' },
-                    { time: '15:10 - 16:45', subject: 'РБЗ', teacher: 'Матвеев М.Г.', classroom: 'ДО' },
-                    { time: '16:55 - 18:30', subject: 'SAP R3', teacher: 'Илларионов И.В.', classroom: 'ДО' },
-                    { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null },
-                    { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null },
-                ]
-
-            },
-            {
-                day: 'Среда', numerator:
-                    [
-                        { time: '8:00 - 9:35', subject: 'ИСиС', teacher: 'Коваль А.С.', classroom: '316п' },
-                        { time: '9:45 - 11:20', subject: 'ФИЗВОСПИТАНИЕ', teacher: 'Щеглова Е.В.', classroom: 'центральный корпус' },
-                        { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null },
-                        { time: '13:25 - 15:00', subject: 'ЯП Джава', teacher: 'Самойлов Н.К', classroom: 'ДО' },
                         { time: '15:10 - 16:45', subject: 'КТ', teacher: 'Запрягаев С.А.', classroom: 'ДО' },
                         { time: '16:55 - 18:30', subject: 'ИТ', teacher: 'Михайлов Е.М.', classroom: 'ДО' },
                         { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null },
                         { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null },
-
                     ]
-                , denominator: [
 
-                    { time: '8:00 - 9:35', subject: 'Технологии программирования', teacher: 'Зенин К.В.', classroom: '479' },
-                    { time: '9:45 - 11:20', subject: 'Квантовая теория', teacher: 'Стадная Н.П.', classroom: '385' },
-                    { time: '11:30 - 13:05', subject: null, teacher: null, classroom: null },
-                    { time: '13:25 - 15:00', subject: null, teacher: null, classroom: null },
-                    { time: '15:10 - 16:45', subject: 'КТ', teacher: 'Запрягаев С.А.', classroom: 'ДО' },
-                    { time: '16:55 - 18:30', subject: 'ИТ', teacher: 'Михайлов Е.М.', classroom: 'ДО' },
-                    { time: '18:40 - 20:00', subject: null, teacher: null, classroom: null },
-                    { time: '20:10 - 21:30', subject: null, teacher: null, classroom: null },
-                ]
-
-            }
+                }
             ]
         }
     },
